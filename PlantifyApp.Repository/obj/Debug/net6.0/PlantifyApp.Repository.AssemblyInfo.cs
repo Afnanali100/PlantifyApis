2290c624f9e70cb7c16fd6cecc885b52a3a73d27
Afnanali100 <afnanamer100@gamil.com>
2024-04-30 02:43:31 +0300

    [Security Module] & [ Update user Profile for User Module]

--- PlantifyApp.Repository/obj/Debug/net6.0/PlantifyApp.Repository.AssemblyInfo.cs
+++ PlantifyApp.Repository/obj/Debug/net6.0/PlantifyApp.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantifyApp.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db09a33ab2a5f4c67977f98221eae29e953265a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantifyApp.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantifyApp.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
